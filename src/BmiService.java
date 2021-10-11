91a8922d9651c5203813fcb65e99862a7e25ee06
Valeriya Polukhina <valeriapolyxina@mail.ru>
2021-10-11 12:50:32 +0300

    Second Commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,5 @@
 public class BmiService {
-    public double calculate(double weight) {
-        double height = 1.75;
+    public double calculate(double weight, double height) {
         double bmi = (weight / Math.pow(height,2));
         return bmi;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bmi = service.calculate(65.5);
+        double bmi = service.calculate(65.5, 1.75);
         System.out.println("BMI: "+bmi);
     }
 }
